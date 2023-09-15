8bd831f293643453026c44d869257eebfcfe2200
voidrlm <varjun609@gmail.com>
2023-09-16 05:03:59 +0530

    Change in vue config to select spefic organisations environmental files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+const dotenv = require("dotenv"); //Loads env variable files
+//Validation check
+
+let envfile = ".env";
+if (process.env.NODE_ENV) {
+  envfile += "." + process.env.NODE_ENV + ".local"; //CREATING ENV FILE NAME
+}
+//ASSIGNING PATH TO DOTENV
+const result = dotenv.config({
+  path: path.resolve(`organisations/${process.env.VUE_APP_DIRECTORY}`, envfile),
+});
+
+// THROWS DOTENV ERROR IN TERMINAL
+if (result.error) {
+  throw result.error;
+}
 
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
