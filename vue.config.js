fd0ea9954965c308c927d182a3bb84eecfd22325
voidrlm <varjun609@gmail.com>
2023-09-16 05:04:26 +0530

    Added a validation check in vue config

--- vue.config.js
+++ vue.config.js
@@ -2,21 +2,27 @@ const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 const dotenv = require("dotenv"); //Loads env variable files
 //Validation check
+if (
+  typeof process.env.VUE_APP_DIRECTORY === "string" &&
+  process.env.VUE_APP_DIRECTORY !== ""
+) {
+  let envfile = ".env";
+  if (process.env.NODE_ENV) {
+    envfile += "." + process.env.NODE_ENV + ".local"; //CREATING ENV FILE NAME
+  }
+  //ASSIGNING PATH TO DOTENV
+  const result = dotenv.config({
+    path: path.resolve(
+      `organisations/${process.env.VUE_APP_DIRECTORY}`,
+      envfile
+    ),
+  });
 
-let envfile = ".env";
-if (process.env.NODE_ENV) {
-  envfile += "." + process.env.NODE_ENV + ".local"; //CREATING ENV FILE NAME
+  // THROWS DOTENV ERROR IN TERMINAL
+  if (result.error) {
+    throw result.error;
+  }
 }
-//ASSIGNING PATH TO DOTENV
-const result = dotenv.config({
-  path: path.resolve(`organisations/${process.env.VUE_APP_DIRECTORY}`, envfile),
-});
-
-// THROWS DOTENV ERROR IN TERMINAL
-if (result.error) {
-  throw result.error;
-}
-
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
 });
