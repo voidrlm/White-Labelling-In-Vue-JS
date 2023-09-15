8b01e16b17af377827e194492a4939486d950340
voidrlm <varjun609@gmail.com>
2023-09-16 04:57:46 +0530

    Simple vue js project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-main>
+      <dashboard />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import dashboard from "./views/dashboard.vue";
+
+export default {
+  name: "App",
+  components: {
+    dashboard,
+  },
+  data: () => ({
+    //
+  }),
+  mounted() {},
+};
+</script>
